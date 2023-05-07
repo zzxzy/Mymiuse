96ea2b28664b9e8698e35c68de3fb880e79e6f1d
zzxzy <92174397+zzxzy@users.noreply.github.com>
2023-05-07 20:18:40 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,93 +7,93 @@ import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-   
-    defineConfig({
-        plugins: [
-            vue(),
-            AutoImport({
-                imports: [
-                    "vue",
-                    {
-                        "naive-ui": [
-                            "useDialog",
-                            "useMessage",
-                            "useNotification",
-                            "useLoadingBar",
-                        ],
-                    },
-                ],
-            }),
-            Components({
-                resolvers: [NaiveUiResolver()],
-            }),
-            // PWA
-            VitePWA({
-                registerType: "autoUpdate",
-                workbox: {
-                    clientsClaim: true,
-                    skipWaiting: true,
-                    cleanupOutdatedCaches: true,
-                    runtimeCaching: [{
-                            urlPattern: /(.*?)\.(woff2|woff|ttf)/,
-                            handler: "CacheFirst",
-                            options: {
-                                cacheName: "file-cache",
-                            },
-                        },
-                        {
-                            urlPattern: /(.*?)\.(webp|png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/,
-                            handler: "CacheFirst",
-                            options: {
-                                cacheName: "image-cache",
-                            },
-                        },
-                    ],
-                },
-                manifest: {
-                    name: "SPlayer",
-                    short_name: "SPlayer",
-                    description: "一个简约的在线音乐播放器",
-                    display: "standalone",
-                    start_url: "/",
-                    theme_color: "#fff",
-                    background_color: "#efefef",
-                    icons: [{
-                        src: "/images/logo/favicon.png",
-                        sizes: "200x200",
-                        type: "image/png",
-                    }, ],
-                },
-            }),
+export default ({ mode }) =>
+  defineConfig({
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: [
+          "vue",
+          {
+            "naive-ui": [
+              "useDialog",
+              "useMessage",
+              "useNotification",
+              "useLoadingBar",
+            ],
+          },
         ],
-        server: {
-            port: 2048,
-            open: true,
-            http: true,
-            ssr: false,
-            proxy: {
-                "/api": {
-                    target: loadEnv(mode, process.cwd()).VITE_API_BASE_URL,
-                    changeOrigin: true,
-                    rewrite: (path) => path.replace(/^\/api/, ""),
-                },
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+      }),
+      // PWA
+      VitePWA({
+        registerType: "autoUpdate",
+        workbox: {
+          clientsClaim: true,
+          skipWaiting: true,
+          cleanupOutdatedCaches: true,
+          runtimeCaching: [
+            {
+              urlPattern: /(.*?)\.(woff2|woff|ttf)/,
+              handler: "CacheFirst",
+              options: {
+                cacheName: "file-cache",
+              },
             },
-        },
-        resolve: {
-            alias: {
-                "@": fileURLToPath(new URL("./src",
-                    import.meta.url)),
+            {
+              urlPattern: /(.*?)\.(webp|png|jpe?g|svg|gif|bmp|psd|tiff|tga|eps)/,
+              handler: "CacheFirst",
+              options: {
+                cacheName: "image-cache",
+              },
             },
+          ],
         },
-        build: {
-            minify: "terser",
-            terserOptions: {
-                compress: {
-                    pure_funcs: ["console.log"],
-                },
+        manifest: {
+          name: "SPlayer",
+          short_name: "SPlayer",
+          description: "一个简约的在线音乐播放器",
+          display: "standalone",
+          start_url: "/",
+          theme_color: "#fff",
+          background_color: "#efefef",
+          icons: [
+            {
+              src: "/images/logo/favicon.png",
+              sizes: "200x200",
+              type: "image/png",
             },
-            sourcemap: false,
+          ],
         },
-    });
-}
+      }),
+    ],
+    server: {
+      port: 2048,
+      open: true,
+      http: true,
+      ssr: false,
+      proxy: {
+        "/api": {
+          target: loadEnv(mode, process.cwd()).VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    build: {
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          pure_funcs: ["console.log"],
+        },
+      },
+      sourcemap: false,
+    },
+  });
